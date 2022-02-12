ee0dcf9bd55fe6cb1259cf223039927130441e36
changan-hub <55674654+changan-hub@users.noreply.github.com>
2022-02-12 13:59:32 +0800

    Add files via upload

---
+++ DemoForm/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的一般信息由以下
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+'查看程序集特性的值
+
+<Assembly: AssemblyTitle("DemoForm")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("DemoForm")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+<Assembly: Guid("207793d9-1511-462f-82bc-3b1315ce16cf")>
+
+' 程序集的版本信息由下列四个值组成: 
+'
+'      主版本
+'      次版本
+'      生成号
+'      修订号
+'
+' 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
+' 方法是按如下所示使用“*”: :
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
